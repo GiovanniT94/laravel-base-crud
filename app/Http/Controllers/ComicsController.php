b32d99fed8645f8aac52203926b391ee99c41854
GiovanniT94 <giovanni.t94@hotmail.it>
2023-01-26 22:08:09 +0100

    Es pushato

---
+++ app/Http/Controllers/ComicsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comics;
+use Illuminate\Http\Request;
+
+class ComicsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comics = Comics::paginate(5);
+        return view('comics.index', compact('comics'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('comics.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $listing = new Comics;
+        $listing->title     = $data['title'];
+        $listing->description   = $data['description'];
+        $listing->thumb   = $data['thumb'];
+        $listing->price    = $data['price'];
+        $listing->series    = $data['series'];
+        $listing->sale_date  = $data['sale_date'];
+        $listing->type  = $data['type'];
+        $listing->save();
+
+        return redirect()->route('comics.show', ['comic' => $listing])->with('success_create', true);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Comics  $comics
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comics $comics)
+    {
+        return view('comics.show', compact('comics'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Comics  $comics
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Comics $comics)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Comics  $comics
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comics $comics)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Comics  $comics
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comics $comics)
+    {
+        //
+    }
+}
---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.base')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-12 text-center">
+            <div class="btn btn-success">
+                <a href="/comics/create" class="text-white">Aggiungi fumetto</a>
+            </div>
+        </div>
+    </div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Link</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Data di vendita</th>
+                <th scope="col">Tipo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{ $comic->id }}</th>
+                    <td>{{ $comic->title }}</td>
+                    {{-- <td>{{ $comic->description }}</td> --}}
+                    <td>{{ $comic->thumb }}</td>
+                    <td>{{ $comic->price }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->sale_date }}</td>
+                    <td>{{ $comic->type }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    {{ $comics->links() }}
+@endsection
